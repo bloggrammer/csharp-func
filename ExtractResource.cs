c8bbbdc270a743a6ec619e5ecbc4551634ea884b
John Ansa <john.ansa@cyphercrescent.com>
2021-07-10 11:35:22 +0100

    Add ExtractResource.cs

--- ExtractResource.cs
+++ ExtractResource.cs
@@ -1,10 +1,25 @@
-Public void Extract(string nameSpace, string outDirectory, string internalFilePath, string resourceName)
-        {
-            Assembly assembly = Assembly.GetCallingAssembly();
-            var path = $"{nameSpace}.{(string.IsNullOrWhiteSpace(internalFilePath) ? string.Empty : internalFilePath)}.{resourceName}";
-            using (Stream s =assembly.GetManifestResourceStream(path))
-                using(BinaryReader r =new BinaryReader(s))
-                    using(FileStream fs = new FileStream($"{outDirectory}\\{resourceName}", FileMode.OpenOrCreate))
-                        using (BinaryWriter w = new BinaryWriter(fs))
-                             w.Write(r.ReadBytes((int)s.Length));
-        }
+
+using System.Reflection;
+using System.IO;
+
+public class ExtractResource
+{
+    /// <summary>
+    /// Extract an Embedded Resource from an Assembly
+    /// </summary>
+    /// <param name="nameSpace">Calling assembly namespace</param>
+    /// <param name="outDirectory">output file directory</param>
+    /// <param name="internalFilePath">Resource file path witthin the calling assembly</param>
+    /// <param name="resourceName">Name of resource to be extracted cluing its extension</param>
+    public void Extract(string nameSpace, string outDirectory, string internalFilePath, string resourceName)
+    {
+        Assembly assembly = Assembly.GetCallingAssembly();
+        var path = $"{nameSpace}.{(string.IsNullOrWhiteSpace(internalFilePath) ? string.Empty : internalFilePath)}.{resourceName}";
+        using (Stream s = assembly.GetManifestResourceStream(path))
+        using (BinaryReader r = new BinaryReader(s))
+        using (FileStream fs = new FileStream($"{outDirectory}\\{resourceName}", FileMode.OpenOrCreate))
+        using (BinaryWriter w = new BinaryWriter(fs))
+            w.Write(r.ReadBytes((int)s.Length));
+    }
+}
+
