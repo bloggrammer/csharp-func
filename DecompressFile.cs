15b71f3f84c06e630d4c90ce777b56f7728bcafa
John Ansa <john.ansa@cyphercrescent.com>
2021-08-16 09:27:53 +0100

    Create DecompressFile.cs

---
+++ DecompressFile.cs
@@ -0,0 +1,8 @@
+    public static byte[] Decompress(byte[] data)
+    {
+        MemoryStream input = new MemoryStream(data);
+        MemoryStream output = new MemoryStream();
+        using (DeflateStream dstream = new DeflateStream(input, CompressionMode.Decompress))
+            dstream.CopyTo(output);
+        return output.ToArray();
+    }
